1f4f7abd04b90c3c21878500e23a037c9ec91bfd
nachurboi <nachurboi@gmail.com>
2020-01-07 11:30:24 +0100

    updated

--- getting_system_info.py
+++ getting_system_info.py
@@ -26,14 +26,14 @@ print(f"Version: {uname.version}")
 print(f"Machine: {uname.machine}")
 print(f"Processor: {uname.processor}")
 
-# Boot Time
+# prints the most recent Boot Time
 print("="*40, "Boot Time", "="*40)
 boot_time_timestamp = psutil.boot_time()
 bt = datetime.fromtimestamp(boot_time_timestamp)
 print(f"Boot Time: {bt.year}/{bt.month}/{bt.day} {bt.hour}:{bt.minute}:{bt.second}")
 
 
-# let's print CPU information
+# this section prints CPU information
 print("="*40, "CPU Info", "="*40)
 # number of cores
 print("Physical cores:", psutil.cpu_count(logical=False))
@@ -54,7 +54,7 @@ print(f"Total CPU Usage: {psutil.cpu_percent()}%")
 # Memory Information
 print("="*40, "Memory Information", "="*40)
 
-# get the memory details
+# this section gets and prints  the memory details
 svmem = psutil.virtual_memory()
 print(f"Total: {get_size(svmem.total)}")
 print(f"Available: {get_size(svmem.available)}")
@@ -62,7 +62,7 @@ print(f"Used: {get_size(svmem.used)}")
 print(f"Percentage: {svmem.percent}%")
 print("="*20, "SWAP", "="*20)
 
-# get the swap memory details (if exists)
+# gets and prints the swap memory details (if exists)
 swap = psutil.swap_memory()
 print(f"Total: {get_size(swap.total)}")
 print(f"Free: {get_size(swap.free)}")
@@ -70,11 +70,11 @@ print(f"Used: {get_size(swap.used)}")
 print(f"Percentage: {swap.percent}%")
 
 
-# Disk Information
+# prints the current Disk Information
 print("="*40, "Disk Information", "="*40)
 print("Partitions and Usage:")
 
-# get all disk partitions
+# get all disk partitions and print them to terminal
 partitions = psutil.disk_partitions()
 for partition in partitions:
     print(f"=== Device: {partition.device} ===")
